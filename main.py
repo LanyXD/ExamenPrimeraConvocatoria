a76aec6c64f5d2d9ae044b1e7772b84351968f93
Andy Monzón <andyml2639@gmail.com>
2022-05-03 10:01:28 -0600

    animales en python
    se realizo un pograma pequeño acerca de mi animal favorito

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("Andy Isaac Monzón López - 1530522")
+print("¿Cual es tu animal favorito?")
+animal = input("animal: ")
+
+if animal == "gato":
+    print("a mí también me gustan los gatos")
+else:
+    print("Es un animal interesante, pero me gustan más los gatos")
